b53ee62b48616d4aeae11fc6407697187bffca88
Ronojoy Vinayak Bhatt <ronojoy1092002@gmail.com>
2024-09-14 14:22:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const tabs = document.querySelectorAll('.tab_btn');
+const all_content = document.querySelectorAll('.content');
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', (e) => {
+        tabs.forEach(tab => { tab.classList.remove('active') });
+        tab.classList.add('active');
+
+        let line = document.querySelector('.line');
+        line.style.width = e.target.offsetWidth + "px";
+        line.style.left = e.target.offsetLeft + "px";
+
+        all_content.forEach(content=>{content.classList.remove('active')});
+        all_content[index].classList.add('active');
+    })
+})
+
+
+const menuToggle = document.getElementById('menu-toggle');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuToggle.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
+
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html','./webdev.html','./softwaredev.html','./gamedev.html','./appdev.html','./animation.html','./ai.html','./contactme.html','./hireme.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
